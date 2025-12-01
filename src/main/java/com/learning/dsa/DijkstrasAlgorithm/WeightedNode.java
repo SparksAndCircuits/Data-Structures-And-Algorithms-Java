656cdee089c25dc65c5521b0586c4f249964e158
SparksAndCircuits <warrenandrade@live.com>
2025-11-30 21:42:04 -0500

    DijkstrasAlgorithm

---
+++ src/main/java/com/learning/dsa/DijkstrasAlgorithm/WeightedNode.java
@@ -0,0 +1,30 @@
+package com.learning.dsa.DijkstrasAlgorithm;
+
+import java.util.*;
+
+public class WeightedNode implements Comparable<WeightedNode> {
+    public String name;
+    public ArrayList<WeightedNode> neighbours = new ArrayList<>();
+    public HashMap<WeightedNode, Integer> weightMap = new HashMap<>();
+    public boolean isVisited = false;
+    public WeightedNode parents;
+    public int distance;
+    public int index;
+    public Object parent;
+
+    public WeightedNode(String name, int index) {
+        this.name = name;
+        distance = Integer.MAX_VALUE;
+        this.index = index;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(WeightedNode other) {
+        return this.distance = other.distance;
+    }
+}
